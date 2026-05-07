622d2150a9598bd361167510fef712470ff71b12
Alessandro Giustina <alexjusti67n@gmail.com>
2026-05-07 18:14:36 +0200

    chore: modified names

--- include/timebase/timebase-api.h
+++ include/timebase/timebase-api.h
@@ -20,14 +20,14 @@
  * \retval TIMEBASE_RC_NULL_POINTER if a tasks is not implemented
  * \retval TIMEBASE_RC_OK otherwise
  */
-enum TimebaseReturnCode timebase_api_init(struct TimebaseHandler *handler, const uint32_t resolution_ms);
+enum TimebaseReturnCode timebase_api_init(struct TimebaseHandler *timebase_handler, const uint32_t resolution_ms);
 
 /*!
  * \brief Enable or disable the timebase
  *
  * \param enabled True to enable the timebase false to disable it
  */
-void timebase_set_enable(struct TimebaseHandler *handler, const bool enabled);
+void timebase_set_enable(struct TimebaseHandler *timebase_handler, const bool enabled);
 
 /*!
  * \brief Increment the internal timebase by one tick
@@ -35,27 +35,27 @@ void timebase_set_enable(struct TimebaseHandler *handler, const bool enabled);
  * \retval TIMEBASE_RC_DISABLED if the timebase is disabled
  * \retval TIMEBASE_RC_OK otherwise
  */
-enum TimebaseReturnCode timebase_inc_tick(struct TimebaseHandler *handler);
+enum TimebaseReturnCode timebase_inc_tick(struct TimebaseHandler *timebase_handler);
 
 /*!
  * \brief Get the current number of ticks
  *
  * \returns uint32_t The number of ticks
  */
-uint32_t timebase_get_tick(struct TimebaseHandler *handler);
+uint32_t timebase_get_tick(struct TimebaseHandler *timebase_handler);
 
 /*!
  * \brief Get the current elapsed time in ms
  *
  * \returns uint32_t The current elapsed time
  */
-uint32_t timebase_get_time(struct TimebaseHandler *handler);
+uint32_t timebase_get_time(struct TimebaseHandler *timebase_handler);
 
 /*!
  * \brief Get the number of ms that represents a single tick
  *
  * \returns uint32_t The timebase resolution
  */
-uint32_t timebase_get_resolution(struct TimebaseHandler *handler);
+uint32_t timebase_get_resolution(struct TimebaseHandler *timebase_handler);
 
 #endif /* TIMEBASE_API_H */
