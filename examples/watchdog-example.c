3464a1693badf0c82c1e444a21d006a953c409db
Alessandro Giustina <alexjusti67n@gmail.com>
2026-05-08 20:55:46 +0200

    feat: added examples for watchdog module

---
+++ examples/watchdog-example.c
@@ -0,0 +1,154 @@
+/*!
+ * \file watchdog-example.c
+ * \date 2026-05-07
+ * \authors Alessandro Giustina [giustinalessandro@gmail.com]
+ *
+ * \brief Simple example of the watchdog module usage.
+ * \details In this example, we initialize the watchdog system and create three simple watchdog instances.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "watchdogs-api.h"
+#include <inttypes.h>
+
+void print_watchdog_1(void) {
+    printf("Watchdog_1 executed");
+    return;
+}
+
+void print_watchdog_2(void) {
+    printf("Watchdog_2 executed");
+    return;
+}
+
+void print_watchdog_3(void) {
+    printf("Watchdog_3 executed");
+    return;
+}
+
+int main(void) {
+
+    /*
+     * Initialize the watchdog module with some watchdogs
+     */
+
+    struct WatchdogHandler watchdogs_handler;
+
+    watchdogs_api_init_module(&watchdogs_handler, 0U);
+
+    struct Watchdog watchdog_1;
+    struct Watchdog watchdog_2;
+    struct Watchdog watchdog_3;
+
+    watchdogs_api_init_watchdog(&watchdog_1, 5U, print_watchdog_1);
+    watchdogs_api_init_watchdog(&watchdog_2, 7U, print_watchdog_2);
+    watchdogs_api_init_watchdog(&watchdog_3, 10U, print_watchdog_3);
+
+    watchdogs_api_watchdog_start(&watchdogs_handler, &watchdog_1, 0U);
+    watchdogs_api_watchdog_start(&watchdogs_handler, &watchdog_2, 0U);
+    watchdogs_api_watchdog_start(&watchdogs_handler, &watchdog_3, 0U);
+
+    /*
+     * Go trough some ticks to see initialized behaviour.
+     * We enable the module at tick 5
+     * At tick 10 watchdog 1 should fire
+     * At tick 12 watchdog 2 should fire
+     * At tick 15 watchdog 3 should fire
+     * 
+     */
+
+    for (uint32_t i = 0; i <= 25; i++) {
+
+        if (i == 5) {
+            watchdogs_api_enable_module(&watchdogs_handler, i);
+        }
+
+        printf("Tick: %" PRId32 "  -> ", i);
+        watchdogs_api_routine(&watchdogs_handler, i);
+
+        printf("\n");
+    }
+
+    /*
+     * Pause watchdog 2 at tick 30 and see that it doesn't fire at tick 26
+     * and then pause it at tick 35, at tick 40 unpase and it should fire after 2 ticks
+     * 
+     * At tick 35 restart also watchdog 3 that should fire after 10 ticks
+     */
+
+    for (uint32_t i = 26; i <= 60; i++) {
+
+        if (i == 30) {
+            watchdogs_api_watchdog_restart(&watchdogs_handler, &watchdog_2, i);
+            watchdogs_api_watchdog_restart(&watchdogs_handler, &watchdog_3, i);
+        }
+
+        printf("Tick: %" PRId32 "  -> ", i);
+        watchdogs_api_routine(&watchdogs_handler, i);
+
+        if (i == 35) {
+            watchdogs_api_watchdog_pause(&watchdogs_handler, &watchdog_2, i);
+        }
+
+        printf("\n");
+    }
+
+    /*
+     * If the module is disabled at tick 64, no watchdog should fire at tick 70, 
+     * but if we reenable it at tick 75 all the watchdogs should resume as if they were frozen.
+     * 
+     * watchdog 1 should fire at tick 76, watchdog 2 at tick 78 and watchdog 3 at tick 81
+     */
+
+    watchdogs_api_watchdog_restart(&watchdogs_handler, &watchdog_1, 60U);
+    watchdogs_api_watchdog_restart(&watchdogs_handler, &watchdog_2, 60U);
+    watchdogs_api_watchdog_restart(&watchdogs_handler, &watchdog_3, 60U);
+
+    for (uint32_t i = 61; i <= 100; i++) {
+
+        if (i == 64) {
+            watchdogs_api_disable_module(&watchdogs_handler, i);
+        }
+
+        if (i == 75) {
+            watchdogs_api_enable_module(&watchdogs_handler, i);
+        }
+
+        printf("Tick: %" PRId32 "  -> ", i);
+        watchdogs_api_routine(&watchdogs_handler, i);
+
+        printf("\n");
+    }
+
+    /*
+     * We can see the same behaviour if the tick doesn't have a regular increment
+     */
+
+    /*
+     * Expected behaviour:
+     * Then we increment the tick by a random amount between 1 and 5 until tick 130
+     * Then watchdog 1 should fire every 5 ticks and watchdog 2 every 7 ticks.
+     * At tick 100 all watchdogs are restarted
+     */
+
+    watchdogs_api_watchdog_restart(&watchdogs_handler, &watchdog_1, 100U);
+    watchdogs_api_watchdog_restart(&watchdogs_handler, &watchdog_2, 100U);
+    watchdogs_api_watchdog_restart(&watchdogs_handler, &watchdog_3, 100U);
+
+    for (uint32_t i = 100; i <= 130;) {
+
+        printf("Tick: %" PRIu32 "  -> ", i);
+        watchdogs_api_routine(&watchdogs_handler, i);
+
+        printf("\n");
+
+        // Increment tick by a random amount between 1 and 5
+        i += rand() % 3 + 1;
+    }
+
+    return 0;
+}
